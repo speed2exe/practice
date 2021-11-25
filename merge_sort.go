56a688456dfecca942cf207beeccb7ec004f49b5
Fu Zi Xiang <zi.fu@bytedance.com>
2021-11-25 18:44:48 +0800

    update merge sort

--- merge_sort.go
+++ merge_sort.go
@@ -1,66 +1,84 @@
 package practice
 
 func MergeSort(nums []int) []int {
-	if nums == nil {
-		return nil
-	}
-	if len(nums) == 0 {
-		return []int{}
-	}
-	if len(nums) <= 1 {
-		return []int{nums[0]}
-	}
+    if nums == nil {
+        return nil
+    }
 
-	res := make([]int, len(nums))
-	copy(res, nums)
-
-	mid_i := len(res) / 2
-	left, right := res[:mid_i], res[mid_i:]
-
-	left = sort(left)
-	right = sort(right)
-	return merge(left, right)
+    newNums := make([]int, len(nums))
+    copy(newNums, nums)
+    nums = newNums
+    MergeSortNoCopy(nums)
+    return nums
 }
 
-func sort(nums []int) []int {
+func MergeSortNoCopy(nums []int) {
 	if len(nums) < 2 {
-		return nums
+		return
+	}
+
+	mid_i := len(nums) / 2
+	left, right := nums[:mid_i], nums[mid_i:]
+    left_copy := make([]int, len(left))
+    copy(left_copy, left)
+    right_copy := make([]int, len(right))
+    copy(right_copy, right)
+
+	sort(left_copy)
+	sort(right_copy)
+    merge(nums, left_copy, right_copy)
+	return
+}
+
+func sort(nums []int) {
+	if len(nums) < 2 {
+		return
 	}
 	if len(nums) > 2 {
-		return MergeSort(nums)
+        MergeSortNoCopy(nums)
+        return
 	}
 
 	if nums[0] > nums[1] {
 		nums[0], nums[1] = nums[1], nums[0]
 	}
 
-	return nums
+	return
 }
 
 // merge merges 2 sorted array of numbers
-func merge(nums1 []int, nums2 []int) []int {
-	res := make([]int, 0, len(nums1)+len(nums2))
+func merge(original, nums1, nums2 []int) {
+    original = original[:0]
 
 	for {
 		// if any of the slice is empty, just append everything else to
 		// result and return
 		if len(nums1) == 0 {
-			return append(res, nums2...)
+			original = append(original, nums2...)
+            return
+
 		}
 		if len(nums2) == 0 {
-			return append(res, nums1...)
+			original = append(original, nums1...)
+            return
 		}
 
 		// append first elem of nums1 if it is smaller or equal to
 		// first elem of num2
 		if nums1[0] < nums2[0] {
-			res = append(res, nums1[0])
+			original = append(original, nums1[0])
 			nums1 = nums1[1:]
 			continue
 		}
 
 		// append first elem of nums2 otherwise
-		res = append(res, nums2[0])
+		original = append(original, nums2[0])
 		nums2 = nums2[1:]
 	}
 }
+
+// time complexity:
+
+
+
+// space: 
