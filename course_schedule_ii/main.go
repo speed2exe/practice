1ca2181cae6bad80a54cf7483a929996bd6071d4
Fu Zi Xiang <speed2exe@live.com.sg>
2022-09-18 00:37:21 +0800

    format

--- course_schedule_ii/main.go
+++ course_schedule_ii/main.go
@@ -3,56 +3,57 @@
 package course_schedule_ii
 
 func findOrder(numCourses int, prerequisites [][]int) []int {
-    res := make([]int, 0, numCourses)
+	res := make([]int, 0, numCourses)
 
-    // build graph
-    // first dereference gets the numCourses 
-    // second dereference gets the list of dependencies
-    graph := make([][]int, numCourses)
-    for _, p := range prerequisites {
-        key, val := p[0], p[1]
-        graphVal := graph[key]
-        graph[key] = append(graphVal, val)
-    }
+	// build graph
+	// first dereference gets the numCourses
+	// second dereference gets the list of dependencies
+	graph := make([][]int, numCourses)
+	for _, p := range prerequisites {
+		key, val := p[0], p[1]
+		graphVal := graph[key]
+		graph[key] = append(graphVal, val)
+	}
 
-    visited := make([]Status, numCourses)
+	statusByNode := make([]Status, numCourses)
 
-    for i := 0; i < numCourses; i++ {
-        if !fillOrder(graph, visited, i, &res) {
-            return []int{}
-        }
-    }
+	for i := 0; i < numCourses; i++ {
+		if !fillOrder(graph, statusByNode, i, &res) {
+			return []int{}
+		}
+	}
 
-    return res
+	return res
 }
 
 func fillOrder(graph [][]int, statusByNode []Status, node int, res *[]int) bool {
-    status := statusByNode[node]
-    switch status {
-    case VISITING:
-        return false
-    case VISITED:
-        return true
-    case UNVISITED:
-    default:
-        panic("unreachable")
-    }
-    statusByNode[node] = VISITING
+	status := statusByNode[node]
+	switch status {
+	case VISITING:
+		return false
+	case VISITED:
+		return true
+	case UNVISITED:
+	default:
+		panic("unreachable")
+	}
+	statusByNode[node] = VISITING
 
-    for _, n := range graph[node] {
-        if !fillOrder(graph, statusByNode, n, res) {
-            return false
-        }
-    }
+	for _, n := range graph[node] {
+		if !fillOrder(graph, statusByNode, n, res) {
+			return false
+		}
+	}
 
-    *res = append(*res, node)
-    statusByNode[node] = VISITED
-    return true
+	*res = append(*res, node)
+	statusByNode[node] = VISITED
+	return true
 }
 
 type Status int
+
 const (
-    UNVISITED Status = iota
-    VISITING
-    VISITED
+	UNVISITED Status = iota
+	VISITING
+	VISITED
 )
