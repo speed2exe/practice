ae384e81ba1462aa0d403509dd4e6191475a7016
Fu Zi Xiang <speed2exe@live.com.sg>
2023-06-11 20:13:18 +0800

    added 3 sums

--- leetcode/15/main.go
+++ leetcode/15/main.go
@@ -3,14 +3,30 @@ package main
 
 import "sort"
 
-// TODO
 func threeSum(nums []int) [][]int {
-
 	sort.Ints(nums)
 	var result [][]int
-	for i := 0; i < len(nums)-2; i++ {
+	for i := 0; i < len(nums); i++ {
+		if i > 0 && nums[i] == nums[i-1] {
+			continue
+		}
 
+		j := i + 1
+		k := len(nums) - 1
+		for k > j {
+			sum := nums[i] + nums[j] + nums[k]
+			if sum > 0 {
+				k--
+			} else if sum < 0 {
+				j++
+			} else {
+				result = append(result, []int{nums[i], nums[j], nums[k]})
+				j++
+				for k > j && nums[j-1] == nums[j] {
+					j++
+				}
+			}
+		}
 	}
-
 	return result
 }
